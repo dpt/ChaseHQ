5ba016dcbe43e1a8379364a4f69bf59557bca4a9
David Thomas <dave@davespace.co.uk>
2026-06-29 10:42:33 +0100

    build_height_table: Remove unnccessary wraparound handling

--- C/ChaseHQ/ChaseHQ.c
+++ C/ChaseHQ/ChaseHQ.c
@@ -13839,40 +13839,33 @@ static void build_height_table(chqstate_t *state)
 {
   int       carry = 0;
   u8       *proadbuf_height;      /* was IY */
-  u8       *proadbuf_height_base; // Conv: added
   int       heightbyte;           /* was C */
   int       orig_counter;         /* was A, fast_counter masked for multiply */
   const u8 *pvtab;                /* was HL */
-  const u8 *pvtabbase;            // Conv: added
   int       Cmin;                 /* was C */
   int       Bdash_iterations;     /* was B' */
   u8       *phtab;                /* was DE' */
-  u8       *phtabbase;            // Conv: added
   int       DE_v;                 /* was DE */
   u16       HL_result;            /* was HL */
   u8        A;                    /* was A */
   u8       *pdst;                 /* was HL */
-  u8       *pdstbase;             // Conv: added
   const u8 *phtab2;               /* was DE */
-  const u8 *phtabbase2;           // Conv: added
   int       iterations2;          /* was B */
   s8        res;                  /* Conv: added */
 
-  proadbuf_height_base = proadbuf_height = ROADBUF_FWD2PTR(ROADBUF_HEIGHT_OFFSET);
+  proadbuf_height = ROADBUF_FWD2PTR(ROADBUF_HEIGHT_OFFSET);
 
   // Read the current height byte
   heightbyte = *proadbuf_height;
 
   orig_counter = state->fast_counter & 0xE0;
-  pvtabbase = &persp_y_scale[FAST_COUNTER_PERSP_ROW(state)][0];
-  pvtab = pvtabbase + 1;
+  pvtab = &persp_y_scale[FAST_COUNTER_PERSP_ROW(state)][1];
   Cmin = -multiply(orig_counter, heightbyte);
 
   // EXX - bank
 
   // This builds the look-up table at $E301. Assuming it's a height table.
   Bdash_iterations = 21;
-  phtabbase = &state->height_table[0];
   phtab = &state->height_table[1];
   do {
     DE_v = *pvtab * 2;
@@ -13907,19 +13900,18 @@ static void build_height_table(chqstate_t *state)
 #endif
     }
     A += *pvtab;
-    WRAP_INCREMENT_ASSIGN(pvtab, pvtabbase);
+    pvtab++;
 
     *phtab = A; // Write #REGa to the table at $E3xx
-    WRAP_INCREMENT_ASSIGN(phtab, phtabbase);
-    WRAP_INCREMENT_ASSIGN(proadbuf_height, proadbuf_height_base);
+    phtab++;
+    proadbuf_height++;
   } while (--Bdash_iterations > 0);
 
   *phtab = 0xA0;
 
   // Copy the table to $E336 while setting negative values to 96[?]
-  pdstbase = pdst = &state->clamped_heights[0]; // destination
-  phtabbase2 = &state->height_table[0]; // src
-  phtab2 = phtabbase2 + 1;
+  pdst = &state->clamped_heights[0]; // destination
+  phtab2 = &state->height_table[1]; // src
 
   iterations2 = 21;
   Cmin = 96;
@@ -13928,14 +13920,14 @@ static void build_height_table(chqstate_t *state)
     if (A < Cmin)
       Cmin = A;
     *pdst = Cmin;
-    WRAP_INCREMENT_ASSIGN(pdst, pdstbase); // FIXME: this can't wrap
-    WRAP_INCREMENT_ASSIGN(phtab2, phtabbase2); // FIXME: this can't wrap either
+    pdst++;
+    phtab2++;
   } while (--iterations2 > 0);
 
   Cmin = A = (Cmin + 3) & 0xF8;
   A -= *pdst;
   *pdst = Cmin;
-  WRAP_INCREMENT_ASSIGN(pdst, pdstbase);
+  pdst++;
   *pdst = A;
 }
 
